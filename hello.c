ebc815b919b6c42f0c7a7164d42cc621608d7f20
zala275 <ghanshyamzala171@gmail.com>
2025-12-20 02:54:33 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main () {
-    printf("Hello, GITHUB!\n ");
-    return 0;
-}
+  for(int i=1; i<=10; i++) {
+
+    printf("count: %d\n",i);
+
+  }
+  return 0;
+  } 
+
+
+
+
