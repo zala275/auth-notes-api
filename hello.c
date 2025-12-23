8c5b735380337eae7594f2ebfee82d114e73ceca
zala275 <ghanshyamzala171@gmail.com>
2025-12-23 15:47:09 +0530

    changes

--- hello.c
+++ hello.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
-int main(){
-    int x=7;
+int main() {
+    int x = 7;
 
 
     if (x>5){
-        printf("%d is greater thsn 5");
+        printf("%d is greater thsn 5", x);
 
     }else{
-       printf("%d  is  less than 5 ");
+       printf("%d  is  less than 5 ", x);
 
     }
     return 0;
