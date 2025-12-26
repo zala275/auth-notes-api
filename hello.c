6af1ca017207057b4c930a9997a360983b8ce634
zala275 <ghanshyamzala171@gmail.com>
2025-12-26 16:15:09 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,15 +1,34 @@
 #include<stdio.h>
 
-int main() {
-    int x = 7;
-
-
-    if (x>5){
-        printf("%d is greater thsn 5", x);
-
-    }else{
-       printf("%d  is  less than 5 ", x);
-
+int main(){
+    
+    int choice = 2;
+    
+    switch(choice){
+          case 1:
+            printf(" kemn\n");
+            break;
+        case 2:
+        printf("zala\n");
+        break;
+        case 3:
+            printf("om\n")
+              break;   
+        case 4:
+        printf("ok\n");
+        break;
+        
+        default:
+        printf("error\n");
     }
+    
     return 0;
-    }
+
+}
+
+    
+     
+    
+    
+    
+        } 
